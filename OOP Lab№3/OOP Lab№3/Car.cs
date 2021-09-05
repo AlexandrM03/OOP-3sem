ebc301a9594b0b99bf3205ef2a0f7951c1d48eb7
Alexander <alexandrmozolevskiy@gmail.com>
2021-09-05 19:54:42 +0300

    Start lab №3

---
+++ OOP Lab№3/OOP Lab№3/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_3
+{
+    class Car
+    {
+        static readonly string market;
+
+        private int id;
+        private string brand;
+        private string model;
+        private ushort year;
+        private string color;
+        private uint price;
+        private string number;
+    }
+}
