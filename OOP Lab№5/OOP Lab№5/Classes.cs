baabe111e4c09ffe64fd5a23c1e4df6e574a0ffd
Alexander <alexandrmozolevskiy@gmail.com>
2021-09-10 00:17:13 +0300

    Start

---
+++ OOP Lab№5/OOP Lab№5/Classes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_5
+{
+    class Classes
+    {
+        abstract class Product
+        {
+            protected int countOfProducts = 0;
+            public override string ToString() => GetType().Name;
+        }
+
+        class Goods : Product
+        {
+
+        }
+
+        class Pastry : Goods
+        {
+
+        }
+
+        class Flowers : Goods
+        {
+
+        }
+
+        class Clocks : Goods
+        {
+
+        }
+
+        class Cake : Pastry
+        {
+
+        }
+
+        sealed class Sweets : Pastry
+        {
+
+        }
+    }
+}
