32b03fa79158e63fd23e08be95ef040decd06339
SupruniukE <90096504+SupruniukE@users.noreply.github.com>
2021-10-23 15:23:25 +0300

    Update Program.cs

--- OOP Lab№03/OOP Lab№3/Program.cs
+++ OOP Lab№03/OOP Lab№3/Program.cs
@@ -12,7 +12,7 @@ namespace OOP_Lab_3
                 cars[0] = new Car(1317777, "Porche", "911", 2018, "Gold", 180000, "MB2332-6");
                 cars[1] = new Car(1317777, "Porche", "911", 2018, "Gold", 180000, "MB2332-6");
                 cars[2] = new Car(131344, "Toyota", "820", 2010, "Black", 13000, "MB2FD4-3");
-                cars[3] = new Car(1317723, "Mercedes", "AMG", 2016, "Silver", 40000, "MT2343-5");
+                cars[3] = new Car(1317723, "Mercedes", "AMG GT", 2016, "Silver", 40000, "MT2343-5");
                 cars[4] = new Car(131771, "Reno", "Logan", 1998, "Red", 12000, "MB2FV2-2");
 
                 foreach(Car car in cars)
