0323c308fab7c56224495703949fc36e37828785
Alexander <alexandrmozolevskiy@gmail.com>
2021-10-29 00:13:04 +0300

    DONE

---
+++ OOP Lab№15/OOP Lab№15/OddAndEven.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_15
+{
+    public static class OddAndEven
+    {
+        public static void ByOne()
+        {
+            object locker = new object();
+
+            Thread OddThread = new Thread(new ThreadStart(PrintOdd));
+            Thread EvenThread = new Thread(new ThreadStart(PrintEven));
+            EvenThread.Start();
+            OddThread.Start();
+
+            void PrintOdd()
+            {
+                lock (locker)
+                {
+                    for(int i = 1; i < 10; i += 2)
+                    {
+                        Thread.Sleep(50);
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write($"{i} ");
+                    }
+                }
+            }
+
+            void PrintEven()
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 10; i += 2)
+                    {
+                        Thread.Sleep(50);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{i} ");
+                    }
+                }
+            }
+        }
+
+        public static void Together()
+        {
+            Mutex mutex = new Mutex();
+
+            Thread OddThread = new Thread(new ThreadStart(PrintOdd));
+            Thread EvenThread = new Thread(new ThreadStart(PrintEven));
+            EvenThread.Start();
+            OddThread.Start();
+            EvenThread.Join();
+            OddThread.Join();
+            
+            void PrintOdd()
+            {
+                for (int i = 1; i < 10; i += 2)
+                {
+                    mutex.WaitOne();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"{i} ");
+                    mutex.ReleaseMutex();
+                }
+            }
+
+            void PrintEven()
+            {
+                for (int i = 0; i < 10; i += 2)
+                {
+                    mutex.WaitOne();
+                    Thread.Sleep(50);
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write($"{i} ");
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
---
+++ OOP Lab№15/OOP Lab№15/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace OOP_Lab_15
+{
+    class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Process[] processList = Process.GetProcesses();
+
+            using (StreamWriter sw = new StreamWriter(@"D:\2 курс\Лабораторные по ООП\OOP Lab№15\OOP Lab№15\processes.txt"))
+            {
+                sw.WriteLine("{0, -10} {1, -40} {2, -10}", "|ID|", "|Name|", "|Priority|");
+                sw.WriteLine(new string('-', 62));
+                foreach (Process p in processList)
+                {
+                    sw.WriteLine("{0, -10} {1, -40} {2, -10}", p.Id, p.ProcessName, p.BasePriority);
+                }
+            }
+
+            AppDomain thisAppDomain = AppDomain.CurrentDomain;
+            Console.WriteLine($"Name: {thisAppDomain.FriendlyName}");
+            Console.WriteLine($"Details: {thisAppDomain.SetupInformation}");
+            Console.WriteLine($"Assemblies: ");
+            thisAppDomain.GetAssemblies().ToList().ForEach(x => Console.WriteLine($"\t{x.FullName}"));
+
+            AppDomain newDomain = AppDomain.CreateDomain("New Domain");
+            newDomain.Load(Assembly.GetExecutingAssembly().FullName);
+            AppDomain.Unload(newDomain);
+
+
+            void Count(object num)
+            {
+                int number = (int)num;
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(100);
+                }
+            }
+            Thread numbersThread = new Thread(new ParameterizedThreadStart(Count));
+            numbersThread.Name = "myThread";
+
+            numbersThread.Start(9);
+            Thread.Sleep(500);
+            numbersThread.Suspend();
+
+            Console.WriteLine($"Status: {numbersThread.ThreadState}");
+            Console.WriteLine($"Name: {numbersThread.Name}");
+            Console.WriteLine($"Priority: {numbersThread.Priority}");
+            Console.WriteLine($"ID: {numbersThread.ManagedThreadId}");
+
+            numbersThread.Resume();
+            Thread.Sleep(500);
+            Console.WriteLine();
+
+            OddAndEven.ByOne();
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            OddAndEven.Together();
+            Thread.Sleep(1000);
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            TimerCallback timerCallback = new TimerCallback(CurrentTime);
+            Timer timer = new Timer(timerCallback, null, 0, 1000);
+            Thread.Sleep(5000);
+            void CurrentTime(object obj)
+            {
+                Console.WriteLine(DateTime.Now);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
